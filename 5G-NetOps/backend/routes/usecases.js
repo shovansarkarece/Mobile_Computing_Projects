234c98405ef9255af6f400b8a535798392183207
Suva Sarkar <shovansarkar319@gmail.com>
2026-02-17 01:58:48 +0100

    Delete 5G-NetOps directory

--- 5G-NetOps/backend/routes/usecases.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { activateUseCases } from "../controllers/usecaseController.js";
-
-const router = express.Router();
-
-router.post("/activate", activateUseCases);
-
-export default router;
